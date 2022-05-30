a5b8e2cd085bb9ede2e1aad4e10efbb595fb8b9f
EduardooPV <eduardoveltroni@hotmail.com>
2022-05-30 11:54:36 -0300

    feat: Adds dependencies and organizes project structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Todolist | Home</title>
+        <meta name="description" content="Todolist EscolaMais" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Todolist</h1>
+    </div>
+  );
+}
